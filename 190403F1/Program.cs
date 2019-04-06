e025549cb89f3c37586fae772794b1df08f9d588
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-06 15:54:35 +0200

    final

--- 190403F1/Program.cs
+++ 190403F1/Program.cs
@@ -1,21 +1,46 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _190403F1
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        #region Private Methods
+
+        private static double MerTaniKozep(double a, double b)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            return Math.Sqrt(a * b);
+        }
+        private static double SzamTaniKozep(double a, double b)
+        {
+            return (a + b) / 2;
+        }
+
+        #endregion Private Methods
+
+        #region Private Fields
+
+        private static readonly Dictionary<char, Func<double, double, double>> dictionary = new Dictionary<char, Func<double, double, double>>();
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+            dictionary.Add('s', SzamTaniKozep);
+            dictionary.Add('m', SzamTaniKozep);
+            double szam1 = AdatBekeres.EllenorzottBekeres<double>("Add meg a számot!");
+            double szam2 = AdatBekeres.EllenorzottBekeres<double>("Add meg a számot!");
+            string input = "Ha számtani közepet szeretnél akkor az ENTER előtt nyomd meg az s billentyűt\n" +
+                "Ha számtani közepet szeretnél akkor az ENTER előtt nyomd meg az m billentyűt";
+            char ch = AdatBekeres.EllenorzottBekeres(dictionary.ContainsKey, input);
+            dictionary.TryGetValue(ch, out Func<double, double, double> func);
+            Console.WriteLine(func(szam1, szam2));
             Console.WriteLine("Hello World!");
             Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
+
+        #endregion Private Methods
     }
 }
